73c6f59097ff03dc5a7424dcdbd3f614ccf4138c
trejdych <rejdychtomasz@me.com>
2022-02-11 10:37:35 +0100

    fix ios content insets

--- packages/webview_flutter/webview_flutter_wkwebview/ios/Classes/FlutterWebView.m
+++ packages/webview_flutter/webview_flutter_wkwebview/ios/Classes/FlutterWebView.m
@@ -45,24 +45,6 @@
 
 @implementation FLTWKWebView
 
-- (void)setFrame:(CGRect)frame {
-  [super setFrame:frame];
-  self.scrollView.contentInset = UIEdgeInsetsZero;
-  // We don't want the contentInsets to be adjusted by iOS, flutter should always take control of
-  // webview's contentInsets.
-  // self.scrollView.contentInset = UIEdgeInsetsZero;
-  if (@available(iOS 11, *)) {
-    // Above iOS 11, adjust contentInset to compensate the adjustedContentInset so the sum will
-    // always be 0.
-    if (UIEdgeInsetsEqualToEdgeInsets(self.scrollView.adjustedContentInset, UIEdgeInsetsZero)) {
-      return;
-    }
-    UIEdgeInsets insetToAdjust = self.scrollView.adjustedContentInset;
-    self.scrollView.contentInset = UIEdgeInsetsMake(-insetToAdjust.top, -insetToAdjust.left,
-                                                    -insetToAdjust.bottom, -insetToAdjust.right);
-  }
-}
-
 @end
 
 @implementation FLTWebViewController {
